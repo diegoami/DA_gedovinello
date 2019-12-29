932fcaced1237401dd9cd36145f712e3c4501bfa
diegoami <diego.amicabile@gmail.com>
2019-12-29 19:33:01 +0100

    Remove dashboard from code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapbrowserComponent } from './mapbrowser/mapbrowser.component';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MapDetailComponent } from './map-detail/map-detail.component';
 
 const routes: Routes = [
   { path: 'mapbrowser', component: MapbrowserComponent },
-  { path: 'dashboard', component: DashboardComponent },
   { path: 'mapdetail/:id/:hotspotfile', component: MapDetailComponent },
   { path: '', redirectTo: '/mapbrowser', pathMatch: 'full' }
 ];
